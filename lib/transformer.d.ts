acf14df60ed593f62d1fc74941e8b95a653b4fc5
diontools <diontools@users.noreply.github.com>
2020-08-23 23:20:50 +0900

    delete lib folder

--- lib/transformer.d.ts
+++
@@ -1,6 +0,0 @@
-import * as ts from 'typescript';
-import 'colors';
-export interface PluginOptions {
-    host?: ts.CompilerHost;
-}
-export default function createTransformer(program: ts.Program, opts?: PluginOptions): ts.TransformerFactory<ts.SourceFile>;
